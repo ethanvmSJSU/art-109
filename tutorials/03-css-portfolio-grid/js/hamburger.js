ec88a096abea70c6fa41337e15d09cf9c61892e8
ethanvmSJSU <ethan.vilchismacedo@sjsu.edu>
2026-03-02 21:53:19 -0800

    portfolio part 2 lmao

---
+++ tutorials/03-css-portfolio-grid/js/hamburger.js
@@ -0,0 +1,8 @@
+// console.log("yoooooo");
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
