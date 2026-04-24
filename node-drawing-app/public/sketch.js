337189185400baae0c4c9d2ee336f9bfc89aef8f
ethanvmSJSU <ethan.vilchismacedo@sjsu.edu>
2026-04-23 18:38:16 -0700

    wahoo updates

---
+++ node-drawing-app/public/sketch.js
@@ -0,0 +1,35 @@
+var socket;
+
+function setup() {
+    createCanvas(400, 400);
+    background(51);
+
+    socket = io.connect("http://localhost:3000");
+    socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data) {
+    noStroke();
+    fill(225, 0, 100);
+    ellipse(data.x, data.y, 60, 60);
+
+}
+
+function mouseDragged() {
+    console.log("Sending: " + mouseX + "," + mouseY);
+
+    var data = {
+        x: mouseX,
+        y: mouseY
+    }
+    socket.emit('mouse', data);
+
+
+    noStroke();
+    fill(225);
+    ellipse(mouseX, mouseY, 60, 60);
+
+}
+function draw() {
+
+}
